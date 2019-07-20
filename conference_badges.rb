38679da9c30a06e8099ab0b5955b030386d7705e
Monique Vitche <mvitche7@gmail.com>
2019-07-20 18:54:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,12 @@ def batch_badge_creator(attendees)
   end
   badges
 end
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each { |badge| puts badge }
+  assign_rooms(attendees).each { |assignment| puts assignment }
+end
